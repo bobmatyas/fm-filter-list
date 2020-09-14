e76ce3208b18992c37c0147fd99201937e17bc14
bob matyas <45246438+bobmatyas@users.noreply.github.com>
2020-09-14 16:10:21 -0400

    setup header component

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import MobileBg from './images/bg-header-mobile.svg';
+import DesktopBg from './images/bg-header-desktop.svg';
 
 const HeaderStyled = styled.header`
-    background-color: hsl(180, 8%, 52%);
+    background-color: hsl(180, 29%, 50%);
     background-image: url(${MobileBg});
     background-size: cover;
     background-repeat: no-repeat;
     height: 155px;
     margin: 0;
     padding: 0;
+
+    @media (min-width: 600px) {
+      background-image: url(${DesktopBg});
+    }
 `;
 
 
