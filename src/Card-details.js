b3f47bb1f0b252b5a57adf2e97f52f3783aed57d
bob matyas <45246438+bobmatyas@users.noreply.github.com>
2020-09-16 15:36:09 -0400

    update css in filter component

--- src/Card-details.js
+++ src/Card-details.js
@@ -2,15 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ListStyled = styled.ul`
-    color: hsl(180, 14%, 20%);
-    font-size: 15px;
+    display: flex;
+    color: hsl(180, 8%, 52%);
+    font-size: 12px;
+    margin: 0;
+    padding: 0;
+
+    li:first-of-type {
+        list-style-type: none;
+        margin-left: 0;
+        padding-left: 0;
+    }
+`;
+
+const ListItem = styled.li`
+    margin: 0 10px;
+    padding: 0 15px;
 `;
 
 
 function Details(props) {
   return (
     <ListStyled>
-        {props.title}
+        <ListItem>1d ago</ListItem>
+        <ListItem>Full Time</ListItem>
+        <ListItem>USA Only</ListItem>
     </ListStyled>
     );
 }
